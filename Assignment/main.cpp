67a8596fee4b5c3010c0b33a6414290c2cf3a29a
harjaus <harjaus@coventry.ac.uk>
2020-02-16 21:18:02 +0000

    All fixing threads working. Optimized code.

--- Assignment/main.cpp
+++ Assignment/main.cpp
@@ -15,10 +15,9 @@ std::condition_variable cv;
 
 int sensorTurn = 0; 
 int numberOfSensors = 6; 
+int lastActivatedSensor = 0;
 
 int loops = 1;
-bool running = true;
-int lastActivatedSensor = 0;
 bool turnOff = false;
 
 void sensor(int currentSensor)
@@ -34,7 +33,6 @@ void sensor(int currentSensor)
 
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
         sensorTurn += 1;  sensorTurn %= numberOfSensors; //to loop back from 6 to 0
-        
 
         int sensorGenerator = rand() % 10 ;
         //std::cout << "Error code: " << sensorGenerator <<std::endl;
@@ -59,9 +57,9 @@ void sensor(int currentSensor)
 
 void diagnosis()
 {
-    while(running)
+    while(true)
     {
-        std::unique_lock<std::mutex> lck(mtx);
+        std::unique_lock<std::mutex> lck(mtx); //while in any other threads, sleep
         while (!((sensorTurn < 0 && sensorTurn > -20) || turnOff))  cv.wait(lck) ;
         if (turnOff) break; //end of program case
 
@@ -75,12 +73,12 @@ void diagnosis()
                 sensorTurn = -30;
                 break;
             case -11:
-                std::cout << "Sinking" << std::endl;
-                sensorTurn = (lastActivatedSensor+1) %numberOfSensors;
+                std::cout << "Blocked" << std::endl;
+                sensorTurn = -31;
                 break;
             case -12:
-                std::cout << "Blocked" << std::endl; 
-                sensorTurn = (lastActivatedSensor+1) %numberOfSensors;  
+                std::cout << "Sinking" << std::endl; 
+                sensorTurn = -32;
                 break; 
         }
 
@@ -91,7 +89,7 @@ void diagnosis()
 
 void fixFreewWeel()
 {
-    while (running)
+    while (true)
     {
         std::unique_lock<std::mutex> lck(mtx);
         while (!(sensorTurn == -30 || turnOff))  cv.wait(lck);
@@ -102,16 +100,69 @@ void fixFreewWeel()
         if (fixing == 0)
         {
             std::cout<< "Freewheeling fixed. Restarting sensors." << std::endl;
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
         }
         else
         {
             std::cout<< "Need to ask earth. Continuing." << std::endl;
+            //hand control to Earth
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
         }
-        sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
         cv.notify_all();
     }
 }
 
+void fixBlockedWheel()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        while (!(sensorTurn == -31 || turnOff))  cv.wait(lck);
+        if (turnOff) break; //end of program case
+
+        int fixing = rand()%4 ; //50% chance of fixing ourselves
+        std::cout << "Fixing Blocked Wheel. " << fixing <<std::endl;
+        if (fixing < 2)
+        {
+            std::cout<< "Blocked Wheel fixed. Restarting sensors." << std::endl;
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
+        }
+        else
+        {
+            std::cout<< "Need to ask earth. Continuing." << std::endl;
+            //hand control to Earth
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
+        }
+        cv.notify_all();
+    }
+}
+
+void fixSinkingWheel()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        while (!(sensorTurn == -32 || turnOff))  cv.wait(lck);
+        if (turnOff) break; //end of program case
+
+        int fixing = rand()%10 ; //10% chance of fixing ourselves
+        std::cout << "Fixing sinking wheel. " << fixing <<std::endl;
+
+        if (fixing == 0)
+        {
+            std::cout<< "Sinking Wheel fixed. Restarting sensors." << std::endl;
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
+        }
+        else
+        {
+            std::cout<< "Need to ask earth. Continuing." << std::endl;
+            //hand control to Earth
+            sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
+        }
+
+        cv.notify_all();
+    }
+}
 
 int main (void){
     srand(time(NULL));
@@ -119,9 +170,9 @@ int main (void){
     std::thread diagnosisThread(diagnosis);
     // std::thread earthConnection();
 
-    // std::thread fixBlock();
     std::thread fixFreewheelThread(fixFreewWeel);
-    // std::thread fixSink();
+    std::thread fixBlockedWheelThread(fixBlockedWheel);
+    std::thread fixSinkingWheelThread(fixSinkingWheel);
 
     std::thread sensor1(sensor, 0);
     std::thread sensor2(sensor, 1);
@@ -137,12 +188,13 @@ int main (void){
     sensor5.join();
     sensor6.join();
 
-    running = false; //kills remaining threads
-    turnOff = true; //turn_off state
+    turnOff = true; //kills remaining threads
     cv.notify_all(); //notifies remaining condition variables
 
     diagnosisThread.join();
     fixFreewheelThread.join();
+    fixBlockedWheelThread.join();
+    fixSinkingWheelThread.join();
 
     return 0;
 }
