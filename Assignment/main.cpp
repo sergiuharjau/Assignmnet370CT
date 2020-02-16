4c2218dd6f605ae4b0cfa1d1fcbf66eaf00b0be3
harjaus <harjaus@coventry.ac.uk>
2020-02-16 21:07:33 +0000

    Fixed freewheeling. Everything works so far

--- Assignment/main.cpp
+++ Assignment/main.cpp
@@ -19,6 +19,7 @@ int numberOfSensors = 6;
 int loops = 1;
 bool running = true;
 int lastActivatedSensor = 0;
+bool turnOff = false;
 
 void sensor(int currentSensor)
 {
@@ -26,11 +27,7 @@ void sensor(int currentSensor)
     {
         std::unique_lock<std::mutex> lck(mtx);
         while(sensorTurn != currentSensor)
-        {
-            // std::cout << "Sensor that is waiting: " << currentSensor << std:: endl;
-            // std::cout << "Current sensor turn: " << sensorTurn << std::endl;
             cv.wait(lck);
-        }
 
         std::cout << "Sensor: " << currentSensor << std::endl;
         lastActivatedSensor = currentSensor;
@@ -40,7 +37,7 @@ void sensor(int currentSensor)
         
 
         int sensorGenerator = rand() % 10 ;
-        std::cout << "Error code: " << sensorGenerator <<std::endl;
+        //std::cout << "Error code: " << sensorGenerator <<std::endl;
         switch(sensorGenerator)
         {
             case 0: 
@@ -65,51 +62,73 @@ void diagnosis()
     while(running)
     {
         std::unique_lock<std::mutex> lck(mtx);
-        while (sensorTurn >= 0) cv.wait(lck) ;
+        while (!((sensorTurn < 0 && sensorTurn > -20) || turnOff))  cv.wait(lck) ;
+        if (turnOff) break; //end of program case
 
-        if (sensorTurn == -77) break; //end of program case
-
-        std::cout << "In diagnosis! Issue:" << std::endl;
+        std::cout << "In diagnosis! Issue: " ;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         switch(sensorTurn)
         {
             case -10:
                 std::cout << "Freewheeling" << std::endl;
+                sensorTurn = -30;
                 break;
             case -11:
                 std::cout << "Sinking" << std::endl;
+                sensorTurn = (lastActivatedSensor+1) %numberOfSensors;
                 break;
             case -12:
-                std::cout << "Blocked" << std::endl;   
+                std::cout << "Blocked" << std::endl; 
+                sensorTurn = (lastActivatedSensor+1) %numberOfSensors;  
                 break; 
         }
 
-        std::cout << "Fixing issue. Restarting sensors." << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-        sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //start from where we left off
         cv.notify_all();
     }
 
 }
 
+void fixFreewWeel()
+{
+    while (running)
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        while (!(sensorTurn == -30 || turnOff))  cv.wait(lck);
+        if (turnOff) break; //end of program case
+
+        int fixing = rand()%3 ; //30% chance of fixing ourselves
+        std::cout << "Fixing free wheel. " << fixing <<std::endl;
+        if (fixing == 0)
+        {
+            std::cout<< "Freewheeling fixed. Restarting sensors." << std::endl;
+        }
+        else
+        {
+            std::cout<< "Need to ask earth. Continuing." << std::endl;
+        }
+        sensorTurn = (lastActivatedSensor+1) %numberOfSensors; //restart sensor, we've fixed the issue
+        cv.notify_all();
+    }
+}
+
 
 int main (void){
     srand(time(NULL));
+
+    std::thread diagnosisThread(diagnosis);
+    // std::thread earthConnection();
+
+    // std::thread fixBlock();
+    std::thread fixFreewheelThread(fixFreewWeel);
+    // std::thread fixSink();
+
     std::thread sensor1(sensor, 0);
     std::thread sensor2(sensor, 1);
     std::thread sensor3(sensor, 2);
     std::thread sensor4(sensor, 3);
     std::thread sensor5(sensor, 4);
     std::thread sensor6(sensor, 5);
-
-    std::thread diagnosisThread(diagnosis);
-    // std::thread earthConnection();
-
-    // std::thread fixBlock();
-    // std::thread fixFreewheel();
-    // std::thread fixSink();
 
     sensor1.join();
     sensor2.join();
@@ -119,10 +138,11 @@ int main (void){
     sensor6.join();
 
     running = false; //kills remaining threads
-    sensorTurn = -77; //turn_off state
+    turnOff = true; //turn_off state
     cv.notify_all(); //notifies remaining condition variables
 
     diagnosisThread.join();
+    fixFreewheelThread.join();
 
     return 0;
 }
