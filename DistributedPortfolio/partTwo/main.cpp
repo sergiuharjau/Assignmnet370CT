3321661cfc03587bba4c864c29469b2676a002af
harjaus <harjaus@coventry.ac.uk>
2020-04-02 16:44:07 +0100

    Tidied up codebase

--- DistributedPortfolio/partTwo/main.cpp
+++ DistributedPortfolio/partTwo/main.cpp
@@ -33,73 +33,68 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Get_processor_name(node_name, &namelen);
-
   memset(node_name+namelen,0,MPI_MAX_PROCESSOR_NAME-namelen);
-  int src = 0; //atoi(argv[1]);
+  int src = 0; //Head Node
 
-  std::vector<std::string> wholePoem = readPoem();
+  std::vector<std::string> wholePoem = readPoem(); //Load Poem
   
-    if (rank == src) //Head node sends out lines
+  if (rank == src) //Head node sends out lines
+  {
+    std::cout << "\nThis is the head node. " << node_name << std::endl;
+
+    #pragma omp parallel for schedule (static,1) //If you comment this line, output is even nicer
+    for (int i = 0; i < size; i ++)
     {
-
-        std::cout << "\nThis is the head node. " << node_name << std::endl;
-
-//        #pragma omp parallel for schedule (static,1) //If you comment this line, output is even nicer
-        for (int i = 0; i < size; i ++)
-        {
-            int line_to_do = size -i -1;
-            std::cout << "\nSending the line: " << wholePoem[line_to_do] << std::endl;
-            std::cout << "To node: " << i << std::endl;
-            MPI_Send(&line_to_do, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-                    //node 0 gets last poem line and so on
-        }
-    std::cout << "\nMoving on to jumbling.\n" << std::endl;
+      int line_to_do = size -i -1;
+      std::cout << "\nSending the line: " << wholePoem[line_to_do] << std::endl;
+      std::cout << "To node: " << i << std::endl;
+      MPI_Send(&line_to_do, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+              //node 0 gets last poem line and so on
     }
+    std::cout << "\nMoving on to jumbling.\n" << std::endl;
+  }
 
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-  
-
   for (int i=0 ; i < size ; i++)
-  {
-    if(rank == i){   
-        int poemLine = -1;
-                //Head node sends out all the lines
-        MPI_Recv(&poemLine, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        
-        if (poemLine==-1) continue;
+  { // loop through different nodes
+    if(rank == i){ //when we get to ourselves
+      int poemLine = -1;
+            //Head node sends out all the lines
+      MPI_Recv(&poemLine, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      
+      if (poemLine == -1) 
+        continue; //just in case something goes wrong
 
-        std::cout << i <<". We are node: " << i << std::endl;
-        std::string line = wholePoem[poemLine]; 
-        std::cout  <<" "<< i <<". Original: " <<  line << std::endl;
+      std::cout << i <<". We are node: " << i << std::endl; // Pretty Output
+      std::string line = wholePoem[poemLine]; 
+      std::cout  <<" "<< i <<". Original: " <<  line << std::endl;
 
-        std::string word;
-        std::vector<std::string> wordsVector ;
+      std::string word;
+      std::vector<std::string> wordsVector; //Separate Words
 
-        for(auto individual : line){
-            if (individual != ' ')
-                word = word + individual ; 
-            else
-            {
-                word = word + " ";
-                wordsVector.emplace_back(word) ;
-                word = "" ;
-            }    
-        }
-         wordsVector.emplace_back(word) ;
-
-        std::random_shuffle ( wordsVector.begin(), wordsVector.end() );
-
-        std::cout << " " << i << ". Shuffled: " ;
-        for (std::string word : wordsVector)
+      for(auto individual : line)
+      {
+        if (individual != ' ')
+          word = word + individual ; 
+        else
         {
-            std::cout << word;
-        }
-        std::cout << std::endl;
+          word = word + " ";
+          wordsVector.emplace_back(word) ;
+          word = "" ;
+        }    
+      }
+      wordsVector.emplace_back(word) ; //deals with last word
 
+      std::random_shuffle ( wordsVector.begin(), wordsVector.end() ); // Jumbling
+
+      std::cout << " " << i << ". Shuffled: " ;
+      for (std::string word : wordsVector)
+        std::cout << word; //Show they are truly jumbled.
+
+      std::cout << std::endl << std::endl;
     }
   }
-
   MPI_Finalize();
 }
 
